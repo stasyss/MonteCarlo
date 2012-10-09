4f2dd3774b51acd7f074230c76eb1a05dba90bff
Stasys Jurksaitis <stasys.jurksaitis@gmail.com>
2012-10-09 18:12:24 +0300

    Smulkūs pakeitimai

--- main.cpp
+++ main.cpp
@@ -4,5 +4,8 @@ using namespace std;
 int main (int argc, char const *argv[])
 {
 	cout << "Montekarlo realizacijos pradžia" << endl;
+	//kintamųjų apsirašymas
+	double f, x; //funkcijos reikšmė ir argumentas x
+	int n; //uždavinio dimensija
 	return 0;
 }
