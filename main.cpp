dc1426aa4a0f8849d1f3fd2e54c0ea3fe309e81a
Stasys Jurksaitis <stasys.jurksaitis@gmail.com>
2012-10-23 19:20:10 +0300

    Relizuotas f-jos min radimas

--- main.cpp
+++ main.cpp
@@ -26,38 +26,56 @@ int main (int argc, char const *argv[])
 	int n; //uždavinio dimensija
 	double xsprendinys; //lala
 	int fsprendinys; //uždavinio sprendinys
-	double a, b, mas[100];
-	int k = 100;
+	double a, b, x0, x1, min=1000000, mas[100];
+	int k = 100000;
 	
 
-	
+	/*
 	cout << "Įveskite intervalo [a,b] rėžių reikšmes" <<endl;
 	cout << "a = "; cin >>a;
 	cout <<endl<< "b = "; cin >>b;
 	cout <<endl<< "vektoriaus dimensiją (n): "; cin >>n;
 	cout << endl;
-	
+	*/
 	srand(time(0));
 	
-	
-	for(int i = 0; i<n; i++)
+	for(int j = 0; j < k; j++)
 	{
-		mas[i] = closed_interval_rand(a, b);
+		/*
+		for(int i = 0; i<n; i++)
+		{
+			mas[i] = closed_interval_rand(a, b);
+		}
+	
+	
+	
+		
+		cout << "x = ( ";
+		for(int i = 0; i<n; i++)
+		{
+			cout<< mas[i]<< " ";
+		}
+	
+		cout << ")"<<endl;
+		*/
+		//mas[0] = -0.089842;
+		//mas[1] = 0.712656;
+		mas[0] = closed_interval_rand(-1.9, 1.9);
+		mas[1] = closed_interval_rand(-1.1, 1.1);
+		xsprendinys = sixhump(&mas[0]);
+		//cout << "Six hump funkcijos reikšmė šiame taške: " << xsprendinys << endl;
+		if(min > xsprendinys)
+		{
+			min = xsprendinys;
+			x0 = mas[0];
+			x1 = mas[1];
+		}
+		
 	}
-	
-	
-	
-	cout << "x = ( ";
-	for(int i = 0; i<n; i++)
-	{
-		cout<< mas[i]<< " ";
-	}
-	
-	cout << ")"<<endl;
-	
-	//mas[0] = -0.089842;
-	//mas[1] = 0.712656;
-	cout << "Six hump funkcijos reikšmė šiame taške: " << sixhump(&mas[0])<< endl;
+	cout<< "====================================="<<endl;
+	cout << "F-jos minimumas yra "<< min <<endl;
+	cout << "taške ("<< x0 <<", "<<x1<< ")"<<endl;
+	cout<< "====================================="<<endl;
 	
 	return 0;
 }
