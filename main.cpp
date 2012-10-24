8391a4a4a860bbf0ccbdda360ae14b8ea5cf56cc
Stasys Jurksaitis <stasys.jurksaitis@gmail.com>
2012-10-24 17:39:38 +0300

    Realizuota funkcijos max ir vidurkis

--- main.cpp
+++ main.cpp
@@ -26,55 +26,48 @@ int main (int argc, char const *argv[])
 	int n; //uždavinio dimensija
 	double xsprendinys; //lala
 	int fsprendinys; //uždavinio sprendinys
-	double a, b, x0, x1, min=1000000, mas[100];
+	double a, b, x0_min, x1_min, x0_max, x1_max, min, max, sum = 0, vid, mas[3];
 	int k = 100000;
 	
-
-	/*
-	cout << "Įveskite intervalo [a,b] rėžių reikšmes" <<endl;
-	cout << "a = "; cin >>a;
-	cout <<endl<< "b = "; cin >>b;
-	cout <<endl<< "vektoriaus dimensiją (n): "; cin >>n;
-	cout << endl;
-	*/
 	srand(time(0));
 	
-	for(int j = 0; j < k; j++)
+	mas[0] = closed_interval_rand(-1.9, 1.9);
+	mas[1] = closed_interval_rand(-1.1, 1.1);
+	xsprendinys = sixhump(&mas[0]);
+	min = xsprendinys;
+	max = xsprendinys;
+	
+	for(int j = 1; j < k; j++)
 	{
-		/*
-		for(int i = 0; i<n; i++)
-		{
-			mas[i] = closed_interval_rand(a, b);
-		}
-	
-	
-	
 		
-		cout << "x = ( ";
-		for(int i = 0; i<n; i++)
-		{
-			cout<< mas[i]<< " ";
-		}
-	
-		cout << ")"<<endl;
-		*/
-		//mas[0] = -0.089842;
-		//mas[1] = 0.712656;
 		mas[0] = closed_interval_rand(-1.9, 1.9);
 		mas[1] = closed_interval_rand(-1.1, 1.1);
 		xsprendinys = sixhump(&mas[0]);
-		//cout << "Six hump funkcijos reikšmė šiame taške: " << xsprendinys << endl;
+		
+		sum += xsprendinys;
+		
 		if(min > xsprendinys)
 		{
 			min = xsprendinys;
-			x0 = mas[0];
-			x1 = mas[1];
+			x0_min = mas[0];
+			x1_min = mas[1];
+		}
+		if(max < xsprendinys)
+		{
+			max = xsprendinys;
+			x0_max = mas[0];
+			x1_max = mas[1];
 		}
 		
 	}
 	cout<< "====================================="<<endl;
 	cout << "F-jos minimumas yra "<< min <<endl;
-	cout << "taške ("<< x0 <<", "<<x1<< ")"<<endl;
+	cout << "taške ("<< x0_min <<", "<<x1_min<< ")"<<endl;
+	cout<< "-------------------------------------"<<endl;
+	cout << "F-jos maximumas yra "<< max <<endl;
+	cout << "taške ("<< x0_max <<", "<<x1_max<< ")"<<endl;
+	cout<< "-------------------------------------"<<endl;
+	cout << "F-jos vidurkis yra "<< (sum/k) <<endl;
 	cout<< "====================================="<<endl;
 	
 	return 0;
