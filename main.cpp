6010adb104c033b98cb530197e88abfca0b255e6
Stasys Jurksaitis <stasys.jurksaitis@gmail.com>
2012-11-01 16:06:19 +0200

    Skaičiavimai atliekami cikle while su eps. paklaida. Naudojamas struktūros masyvas rezultatams laikyti.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <cstdlib>
 #include <ctime> 
 #include <cmath>
+
+#define MAX_MAS 1000000
+#define EPS 0.01
 using namespace std;
 
 double sixhump(double * x)
@@ -19,6 +22,13 @@ double closed_interval_rand(double x0, double x1)
 
 int main (int argc, char const *argv[])
 {
+	struct tsk
+	{
+		double x0;
+		double x1;
+		double f;
+	};
+	
 	cout << "Montekarlo realizacijos pradžia" << endl;
 	cout << "(RANDOM SEARCH METHOD)" << endl;
 	//kintamųjų apsirašymas
@@ -26,23 +36,37 @@ int main (int argc, char const *argv[])
 	int n; //uždavinio dimensija
 	double xsprendinys; //lala
 	int fsprendinys; //uždavinio sprendinys
-	double a, b, x0_min, x1_min, x0_max, x1_max, min, max, sum = 0, vid, mas[3];
-	int k = 100000;
+	double a, b, x0_min, x1_min, x0_max, x1_max, min, max, sum = 0, vid;
+	//double * taskai_x0, * taskai_x1, * f_reiksmes;
+	tsk *reiksmes;
+	double mas[3];
+	
+	
+	reiksmes = (tsk*) malloc (MAX_MAS*sizeof(tsk));
 	
 	srand(time(0));
 	
+	/*
 	mas[0] = closed_interval_rand(-1.9, 1.9);
 	mas[1] = closed_interval_rand(-1.1, 1.1);
 	xsprendinys = sixhump(&mas[0]);
 	min = xsprendinys;
 	max = xsprendinys;
 	
-	for(int j = 1; j < k; j++)
+	taskai_x0[0] = mas[0];
+	taskai_x1[0] = mas[1];
+	f_reiksmes[0] = xsprendinys;
+	
+	for(int j = 1; j < MAX_MAS; j++)
 	{
 		
 		mas[0] = closed_interval_rand(-1.9, 1.9);
 		mas[1] = closed_interval_rand(-1.1, 1.1);
 		xsprendinys = sixhump(&mas[0]);
+		
+		taskai_x0[j] = mas[0];
+		taskai_x1[j] = mas[1];
+		f_reiksmes[j] = xsprendinys;
 		
 		sum += xsprendinys;
 		
@@ -67,8 +91,65 @@ int main (int argc, char const *argv[])
 	cout << "F-jos maximumas yra "<< max <<endl;
 	cout << "taške ("<< x0_max <<", "<<x1_max<< ")"<<endl;
 	cout<< "-------------------------------------"<<endl;
-	cout << "F-jos vidurkis yra "<< (sum/k) <<endl;
+	cout << "F-jos vidurkis yra "<< (sum/MAX_MAS) <<endl;
 	cout<< "====================================="<<endl;
+	*/
+//==============================================================================================	
+	mas[0] = closed_interval_rand(-1.9, 1.9);
+	mas[1] = closed_interval_rand(-1.1, 1.1);
+	xsprendinys = sixhump(&mas[0]);
+	min = xsprendinys;
+	max = xsprendinys;
+	
+	reiksmes[0].x0 = mas[0];
+	reiksmes[0].x1 = mas[1];
+	reiksmes[0].f = xsprendinys;
+	
+	int j=1;
+	int dydis = MAX_MAS;
+	while(abs(-1.031628453 - xsprendinys) >EPS)
+	{
+		
+		mas[0] = closed_interval_rand(-1.9, 1.9);
+		mas[1] = closed_interval_rand(-1.1, 1.1);
+		xsprendinys = sixhump(&mas[0]);
+		
+		reiksmes[j].x0 = mas[0];
+		reiksmes[j].x1 = mas[1];
+		reiksmes[j].f = xsprendinys;
+		
+		sum += xsprendinys;
+		
+		if(min > xsprendinys)
+		{
+			min = xsprendinys;
+			x0_min = mas[0];
+			x1_min = mas[1];
+		}
+		if(max < xsprendinys)
+		{
+			max = xsprendinys;
+			x0_max = mas[0];
+			x1_max = mas[1];
+		}
+		j++;	
+	}
+	
+	cout<< "====================================="<<endl;
+	cout << "F-jos minimumas yra "<< min <<endl;
+	cout << "taške ("<< x0_min <<", "<<x1_min<< ")"<<endl;
+	cout<< "-------------------------------------"<<endl;
+	cout << "F-jos maximumas yra "<< max <<endl;
+	cout << "taške ("<< x0_max <<", "<<x1_max<< ")"<<endl;
+	cout<< "-------------------------------------"<<endl;
+	cout << "F-jos vidurkis yra "<< (sum/(j-1)) <<endl;
+	cout<< "-------------------------------------"<<endl;
+	cout << "Ciklas prasuktas "<< (j-1)<<" kartų" <<endl;
+	cout<< "====================================="<<endl;
+	
+	
+	free (reiksmes);
+
 	
 	return 0;
 }
