e6abae05b6b6d5cce6c30d32e1f11f95afb3ffa5
Stasys Jurksaitis <stasys.jurksaitis@gmail.com>
2012-11-06 19:40:03 +0200

    Vektor + Sorting

--- main.cpp
+++ main.cpp
@@ -2,10 +2,62 @@
 #include <cstdlib>
 #include <ctime> 
 #include <cmath>
+#include <vector>
 
-#define MAX_MAS 1000000
-#define EPS 0.01
+#define EPS 0.00001
 using namespace std;
+
+struct tsk
+	{
+		double x0;
+		double x1;
+		double f;
+	};
+	
+	
+void swap(tsk *x,tsk *y)
+{
+	tsk temp;
+	temp=*x;
+	*x = *y;
+	*y = temp;
+}
+
+void heapsort(tsk *a,int n)
+{
+	int i,s,f;
+	for(i=1;i< n;++i)
+	{
+		s=i;
+		f=(s-1)/2;
+		while( a[f].f< a[s].f)
+		{
+			swap(&a[f],&a[s]);
+			s=f;
+			if(s==0)
+			break;
+			f=(s-1)/2;
+		}
+	}
+	for(i=n-1;i>=1;--i)
+	{
+		swap(&a[0],&a[i]);
+		f=0;
+		s=1;
+		if(i==1)
+		break;
+		if(i>2)if(a[2].f>a[1].f)s=2;
+		while( a[f].f< a[s].f )
+		{
+			swap(&a[f],&a[s]);
+			f=s;
+			s=2*f+1;
+			if(i>s+1 )if(a[s+1].f>a[s].f)s=s+1;
+			if (s>=i)
+			break;
+		}
+	}
+}
 
 double sixhump(double * x)
 {
@@ -22,12 +74,7 @@ double closed_interval_rand(double x0, double x1)
 
 int main (int argc, char const *argv[])
 {
-	struct tsk
-	{
-		double x0;
-		double x1;
-		double f;
-	};
+	
 	
 	cout << "Montekarlo realizacijos pradžia" << endl;
 	cout << "(RANDOM SEARCH METHOD)" << endl;
@@ -38,11 +85,14 @@ int main (int argc, char const *argv[])
 	int fsprendinys; //uždavinio sprendinys
 	double a, b, x0_min, x1_min, x0_max, x1_max, min, max, sum = 0, vid;
 	//double * taskai_x0, * taskai_x1, * f_reiksmes;
-	tsk *reiksmes;
+	//tsk *reiksmes;
+	tsk temp;
 	double mas[3];
 	
+	vector <tsk> reiksmes(0);	
 	
-	reiksmes = (tsk*) malloc (MAX_MAS*sizeof(tsk));
+	//reiksmes = (tsk*) malloc (MAX_MAS*sizeof(tsk));
+	
 	
 	srand(time(0));
 	
@@ -101,12 +151,13 @@ int main (int argc, char const *argv[])
 	min = xsprendinys;
 	max = xsprendinys;
 	
-	reiksmes[0].x0 = mas[0];
-	reiksmes[0].x1 = mas[1];
-	reiksmes[0].f = xsprendinys;
+	temp.x0 = mas[0];
+	temp.x1 = mas[1];
+	temp.f = xsprendinys;
+	
+	reiksmes.push_back(temp);
 	
 	int j=1;
-	int dydis = MAX_MAS;
 	while(abs(-1.031628453 - xsprendinys) >EPS)
 	{
 		
@@ -114,9 +165,11 @@ int main (int argc, char const *argv[])
 		mas[1] = closed_interval_rand(-1.1, 1.1);
 		xsprendinys = sixhump(&mas[0]);
 		
-		reiksmes[j].x0 = mas[0];
-		reiksmes[j].x1 = mas[1];
-		reiksmes[j].f = xsprendinys;
+		temp.x0 = mas[0];
+		temp.x1 = mas[1];
+		temp.f = xsprendinys;
+	
+		reiksmes.push_back(temp);
 		
 		sum += xsprendinys;
 		
@@ -148,8 +201,17 @@ int main (int argc, char const *argv[])
 	cout<< "====================================="<<endl;
 	
 	
-	free (reiksmes);
+	//free (reiksmes);
 
+	heapsort(&reiksmes[0],(j-1));
+	
+	cout<< "=============================================="<<endl;
+	cout << "Trys taškai su minimaliomis f-jos reikšmėmis" <<endl;
+	for(int i=0; i<3; i++)
+	{
+		cout <<"x0="<< reiksmes[i].x0 << " *** x1="<<reiksmes[i].x1<< " *** f="<<reiksmes[i].f<<endl;
+	}
+	cout<< "=============================================="<<endl;
 	
 	return 0;
 }
